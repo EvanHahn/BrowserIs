d211f71e82e6bbd3cb3eb0e6013cdd7c8258f7ed
Evan Hahn <me@evanhahn.com>
2014-10-01 17:27:37 -0400

    Make testing more generic

--- test/test.js
+++ test/test.js
@@ -1,15 +1,32 @@
-var is = require('../');
+var assert = require('assert');
+var is = require('..');
 
 describe('BrowserIs', function() {
 
-  function test(ua) {
-    var request = { headers: { 'user-agent': ua } };
-    return is(request);
-  };
+  function test(name, userAgents, shouldBeTrue) {
 
-  it('detects IE6', function() {
-    var results = test('Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)');
-    results.ie.should.be.true;
-  });
+    it('detects ' + name, function() {
+      userAgents.forEach(function(ua) {
+        var request = { headers: { 'user-agent': ua } };
+        var results = is(request);
+        Object.keys(results).forEach(function(key) {
+          if (shouldBeTrue.indexOf(key) === -1) {
+            assert(!results[key]);
+          } else {
+            assert(results[key]);
+          }
+        });
+      });
+    });
+
+  }
+
+  test('IE6', [
+    'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+    'Mozilla/4.0 (compatible; MSIE 6.0b; Windows NT 5.1)',
+    'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)'
+  ], [
+    'ie', 'ie6', 'desktop', 'windows'
+  ]);
 
 });
